b0a047cff366e7056f18e892baf999eca2ceb577
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-03 15:42:06 -0700

    Created index.js, queries.js, and employeeDB.sql files.  Created table with columns in sql file.

---
+++ employeeDB.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE people (
+id INT AUTO_INCREMENT NOT NULL,
+PRIMARY KEY (id),
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+title VARCHAR(50) NOT NULL,
+department VARCHAR(30) NOT NULL,
+salary INT NOT NULL,
+manager VARCHAR(30)
+);
