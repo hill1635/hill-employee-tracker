52ba8893bc45954a7ee4b10f4dd023971f91f25b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-05 21:55:50 -0700

    newEmployee function added

--- queries.js
+++ queries.js
@@ -11,7 +11,19 @@ function displayDepartment() {
         if (err) throw err;
         console.log(res);
         // connection.end();
+    });
+}
+
+function newEmployee() {
+    var query = 'INSERT INTO people (first_name, last_name, title, department, salary, manager) ';
+    query += 'VALUES ( { first_name: answer.firstName } , { last_name: answer.lastName } , { title = answer.title } , ';
+    query += '{ department: answer.department } , { salary: answer.salary } , { manager: answer.manager } ;';
+    query += 'SELECT * FROM people';
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log(res);
     }
+
 }
 
 module.exports = queries;
